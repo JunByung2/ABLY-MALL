c640d02ed16f8e8bcf5ada1c9f79029ac8353c02
JunByung2 <somk444@naver.com>
2025-12-02 17:07:41 +0900

    07. load products from database

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import ProductCard from "./product-card";
-import { Product } from "@/types/index";
 
-const ProductList = ({ data }: { data: Product[] }) => {
+import ProductCard from "./product-card";
+import { Product } from "@/types";
+
+const ProductList = ({ title, data }: { title: string; data: Product[] }) => {
   return (
-    <>
+    <section className="my-10">
+      <h2 className="h2-bold mb-4">{title}</h2>
       {data.length > 0 ? (
         <div>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
@@ -18,7 +20,7 @@ const ProductList = ({ data }: { data: Product[] }) => {
           <p>No product found</p>
         </div>
       )}
-    </>
+    </section>
   );
 };
 
